7627aac19b87205979879770e3cff43c9e9e3fb4
upi1682 <upi538287@gmail.com>
2025-06-03 15:17:16 +0700

    membuat clas pelajar dengan umur

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -22,6 +22,15 @@ class pekerja :  public orang {
         }
 };
 
+class pelajar :  public orang {
+    public :
+
+        pelajar(int pUmur) :
+            orang(pUmur)
+        {
+            cout << "Pelajar dibuat\n" << endl;
+        }
+};
 
 
 
