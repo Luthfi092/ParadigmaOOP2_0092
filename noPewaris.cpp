1e0fc5679311d5a514efb0c96a1c316dff498f76
upi1682 <upi538287@gmail.com>
2025-06-03 15:36:07 +0700

    contoh aagar nerhasil

--- noPewaris.cpp
+++ noPewaris.cpp
@@ -11,12 +11,23 @@ public:
         }
 };
 
+
+class baseClass  { 
+public:
+        virtual void perkenalan() {
+            cout << "Hallo saya Function dari base class";
+        }
+};
+
 class derivedClass : public baseClass {
     public:
     void perkenalan(){
         cout << "Hallo saya Function dari derived Class";
     }
 };
+
+
+
 
 int main () {
     derivedClass a;
